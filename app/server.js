87a7fbc4a289b6fe2cc3902403f9869cf8ac8d7e
James K Nelson <james@numbat.com.au>
2012-09-21 21:50:13 +0800

    removed websockets from socket.io config

--- app/server.js
+++ app/server.js
@@ -1,1 +1,6 @@
-require('derby').run(__dirname + '/src/bootstrap');
+var derby = require('derby');
+derby.io.configure(function () { 
+  derby.io.set("transports", ["xhr-polling"]); 
+  derby.io.set("polling duration", 10); 
+});
+derby.run(__dirname + '/src/bootstrap');
