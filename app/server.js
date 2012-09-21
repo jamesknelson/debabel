e2e3e5e2e537a48b5579cc35adc4279326a2a5be
James K Nelson <james@numbat.com.au>
2012-09-22 00:07:51 +0800

    removed socket.io transports

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 var derby = require('derby');
 derby.io.configure(function () { 
-  derby.io.set("transports", ["xhr-polling"]); 
+  derby.io.set("transports", []); 
   derby.io.set("polling duration", 10); 
 });
 derby.run(__dirname + '/src/bootstrap');
