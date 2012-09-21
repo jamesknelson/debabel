87c27e0e0b02999d52a040c2bca729368cc32504
James K Nelson <james@numbat.com.au>
2012-09-22 01:05:10 +0800

    swapped out derby.run for a custom runner

--- app/server.js
+++ app/server.js
@@ -1,6 +1,10 @@
-var derby = require('derby');
-derby.io.configure(function () { 
-  derby.io.set("transports", ["xhr-polling"]); 
-  derby.io.set("polling duration", 10); 
-});
-derby.run(__dirname + '/src/bootstrap');
+//var derby = require('derby');
+//derby.io.configure(function () { 
+//  derby.io.set("transports", ["xhr-polling"]); 
+//  derby.io.set("polling duration", 10); 
+//});
+//derby.run(__dirname + '/src/bootstrap');
+
+var port = process.env.PORT || 3000;
+module.exports = server = require('./src/bootstrap').listen(port);
+console.log("Debabel listening on port %d", server.address().port);
